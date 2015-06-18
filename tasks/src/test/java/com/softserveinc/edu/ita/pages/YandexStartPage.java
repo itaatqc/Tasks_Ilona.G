94fdbc198f331d74ddad34484bb1d7d35ce7571e
ilona05 <ilonagoshta@gmail.com>
2015-06-18 14:27:27 +0300

    add log4j

--- tasks/src/test/java/com/softserveinc/edu/ita/pages/YandexStartPage.java
+++ tasks/src/test/java/com/softserveinc/edu/ita/pages/YandexStartPage.java
@@ -6,19 +6,51 @@ import org.openqa.selenium.WebElement;
 import com.softserve.edu.ita.tools.ContextVisible;
 
 public class YandexStartPage {
-	private WebElement weatherLink;
+	private WebElement weather;
+	private WebElement comment;
+	private WebElement tempDay;
+	private WebElement tempNight;
+	private WebElement enterLink;
 
 	public YandexStartPage() {
-		this.weatherLink = ContextVisible.get().getVisibleWebElement(
+		this.weather = ContextVisible.get().getVisibleWebElement(
 				By.partialLinkText("Погода"));
+		this.comment = ContextVisible.get().getVisibleWebElement(
+				By.xpath("//*[@id='widgets-4-0']/div/div/div[1]/a[2]"));
+		this.tempDay = ContextVisible.get().getVisibleWebElement(
+				By.xpath("//*[@id='widgets-4-0']/div/div/div[2]/a[1]"));
+		this.tempNight = ContextVisible.get().getVisibleWebElement(
+				By.xpath("//*[@id='widgets-4-0']/div/div/div[2]/a[2]"));
+		
+		this.enterLink = ContextVisible.get().getVisibleWebElement(
+				By.partialLinkText("Увійти"));
 	}
 
-	public WebElement getWeatherLink() {
-		return this.weatherLink;
+	public WebElement getWeather() {
+		return this.weather;
 	}
-
-	public WeatherPage weatherLinkClick() {
-		this.weatherLink.click();
-		return new WeatherPage();
+	public WebElement getComment() {
+		return this.comment;
+	}
+	public WebElement getTempDay() {
+		return this.tempDay;
+	}
+	public WebElement getTempNight() {
+		return this.tempNight;
+	}
+	public WebElement getEnterLink() {
+		return this.enterLink;
+	}
+	
+	
+//
+//	public WeatherPage weatherLinkClick() {
+//		this.weatherLink.click();
+//		return new WeatherPage();
+//	}
+	
+	public LoginMailPage enterLinkClick() {
+		this.enterLink.click();
+		return new LoginMailPage();
 	}
 }
--- tasks/src/test/java/com/softserveinc/edu/ita/tasks/Weather.java
+++ tasks/src/test/java/com/softserveinc/edu/ita/tasks/Weather.java
@@ -7,11 +7,16 @@ import org.testng.annotations.Test;
 import com.softserve.edu.ita.tools.BrowserRepository;
 import com.softserve.edu.ita.tools.IBrowser;
 import com.softserve.edu.ita.tools.WebDriverUtils;
+import com.softserveinc.edu.ita.pages.LetterPage;
+import com.softserveinc.edu.ita.pages.LoginMailPage;
+import com.softserveinc.edu.ita.pages.MailPage;
 import com.softserveinc.edu.ita.pages.StartPage;
-import com.softserveinc.edu.ita.pages.WeatherPage;
 import com.softserveinc.edu.ita.pages.YandexStartPage;
 
+import org.apache.log4j.Logger;
+
 public class Weather {
+	final static Logger logger = Logger.getLogger(Weather.class);
 
 	@DataProvider
 	public Object[][] firstTest() {
@@ -22,21 +27,34 @@ public class Weather {
 
 	@Test(dataProvider = "firstTest")
 	public void firstTest(IBrowser browser, String url) {
+
 		// Load yandex.ru
 		YandexStartPage yandexStartPage = StartPage.load(browser, url);
 
-		// Click on weatherLink
-		WeatherPage weatherPage = yandexStartPage.weatherLinkClick();
-
 		// Get text of today's weather
 		StringBuilder forecast = new StringBuilder();
-		forecast.append(weatherPage.getDay().getText() + "\n");
-		forecast.append(weatherPage.getComment().getText() + "\n");
-		forecast.append(weatherPage.getTempDay().getText() + "\n");
-		forecast.append(weatherPage.getTempNight().getText() + "\n");
+		forecast.append(yandexStartPage.getWeather().getText() + "\n");
+		forecast.append(yandexStartPage.getComment().getAttribute("title")
+				.toString()+ "\n");
+		forecast.append(yandexStartPage.getTempDay().getText() + "\n");
+		forecast.append(yandexStartPage.getTempNight().getText() + "\n");
+		logger.info("We have got a today's forecast");
+
+		// Click on enterButton
+		LoginMailPage loginMailPage = yandexStartPage.enterLinkClick();
+		logger.info("We have clicked on enterButton");
+
+		// Input login data and Enter into Email
+		MailPage mailPage = loginMailPage.loginMail();
+		logger.info("We have typed login data");
+
+		// Click on writeLetter
+		LetterPage letterPage = mailPage.writeLetterClick();
+		logger.info("We have clicked on writeLetter");
 
 		// Send forecast to email
-		weatherPage.sendMail(forecast);
+		letterPage.sendLetter(forecast);
+		logger.info("We have sent Letter!");
 	}
 
 	// Return to previous state
